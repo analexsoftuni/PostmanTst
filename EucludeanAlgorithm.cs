d525fda144707bef215a01eb72860de8064a185b
Dimitar Plamenov Dimitrov <dimitar.plamenov@gmail.com>
2020-10-01 18:34:57 +0300

    Update EucludeanAlgorithm.cs

--- EucludeanAlgorithm.cs
+++ EucludeanAlgorithm.cs
@@ -19,6 +19,8 @@ namespace EucludeanAlgorithm
                 a = oldB;
             }
             Console.WriteLine("GCD = {0}", a);
+            
+            Console.WriteLine("Test");
         }
     }
 }
